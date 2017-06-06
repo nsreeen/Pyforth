b99d3d5d074d26124e94e3746fece381bf2c9bb1
Nasreen123 <nabuhunaina@protonmail.com>
2017-06-06 11:53:59 +0100

    Add tests

--- forth.py
+++ forth.py
@@ -1,113 +1,120 @@
-# PARAM STACK
+import sys
+
+################### PARAM STACK ###################
 stack = []
 # for now, instead of using a stack pointer, will just use stack[-1]
 # later - maybe add a slot at the start of the dictionary to store this?
+
 def print_stack():
-    print('\n Param stack: ')
-    for slot in reversed(stack):
-        print(slot)
-    print('Return stack: ')
-    for slot in reversed(return_stack):
-        print(slot)
-    print('\n')
+    stri = ""
+    stri = stri + "<" + str(len(stack)) + "> "
+    for item in stack:
+        stri = stri + str(item) + " "
+    print(stri)
 
 def push(item):
-    #print('\n IN PUSH')#, item, PC : ', item, PC)
     stack.append(item)
 
 def pop():
     tos = stack.pop()
-    #print('\n IN POP')#, popped, PC : ', tos, PC)
     return tos
 
-# RETURN stack
+################### RETURN stack ###################
 return_stack = []
-#RP = None
 
 def push_RS(item):
-    print('\n IN PUSH_RS')#, item, PC : ', item, PC)
     return_stack.append(item)
-
 
 def pop_RS():
     tos = return_stack.pop()
-    print('\n IN POP_RS')# , popped, PC : ', tos, PC)
     return tos
 
-# Dictionary
-
+################### Dictionary ###################
+word_trace = [] #for debugging
 STATE = 0 # 0 means interpret mode
-#state could also be in the dictionary? should all (state pc etc) be 
+#state could also be in the dictionary? should all (state pc etc) be? need a way to get them?
 
-# Store HERE, LATEST, PC as first three entries in the dictionary
-dictionary = [-1, -1, -1]
-
-
+# Store HERE, LATEST, PC, index, index2 - as first three entries in the dictionary
+dictionary = ['HERE', -1, 'LATEST', -1, 'PC', -1, 'index', -1, 'index2', -1]
 
 index = None # way to keep track of the 'address' index - work around because using python
 index2 = None
-#dictionary[2] = None
 
-latest = None # will hold link address (index) for previous word once words added
+def print_dictionary(last_section_only=0):
+    if last_section_only == 1:
+        start = len(dictionary) - 15
+    else:
+        start = 0
 
-here = None # holds index of last added to dictionary (prob not necessary in python)
-# not using it now because using append instead
+    for i in range(start, len(dictionary)):
+        cell = dictionary[i]
+        x = ""
+        if cell != 0 and isinstance(cell, int):
+            x = "    | " + str(dictionary[cell])
+        print(i, cell, x)
+
+################### NATIVE FUNCTIONS FOR ADDING TO DICTIONARY  ###################
+
+def comma():
+    tos = pop()
+    dictionary.append(tos)
+    dictionary[1] = len(dictionary) - 1 # HERE - not really necessary as not using it to add
 
 def add_word(name, immediate_flag, code_pointer, data_field):
-    print('IN ADD WORD ', name, '   PC: ', dictionary[2])
-    global latest
-    dictionary.append(name)
-    dictionary.append(immediate_flag)
-    dictionary.append(latest)
-    #print('in add word:  name, len dict :', name, len(dictionary) )
-    placer_for_latest = len(dictionary) - 1 #  index of last cell added (holds link address to previous entry)
+    create(name, immediate_flag) # adds name, imm flag, link address, and updates latest
 
     if code_pointer == variable:
-        dictionary.append(variable)
-        dictionary.append(data_field)
+        push(variable)
+        comma()
+        push(data_field)
+        comma()
 
-    elif code_pointer != None: # tlatest is a link, this is not a composite word
-        dictionary.append(code_pointer)
+    elif code_pointer != None: # it is a link, this is not a composite word
+        push(code_pointer)
+        comma()
 
     else: # this is a composite word
-        dictionary.append(enter) # enter will be code pointer for composite words
+        push(enter)
+        comma()
         for word in data_field:
             push(word)
             find()
-            found = pop()
-            #if found == 0:
-            #print('PROBLEM FINDING WORD - FIND PUSHED 0 TO STACK')
-            link = pop()
-            #print('>>>>>>>>>>> adding word list, word in word list, link found: ', word, link)
-            #print('in add word, adding composite \n found word from word list: ', word, link)
-            dictionary.append(link)
-        dictionary.append(exit)
+            found = pop() #assume it is found for now
+            comma()
 
-    latest = placer_for_latest
+        push(exit)
+        comma()
 
-    here = len(dictionary) - 1
+    dictionary[1] = len(dictionary) - 1 # HERE
 
+def create(name, immediate_flag):
+    push(name)
+    comma()
+    push(immediate_flag)
+    comma()
+    push(dictionary[3]) #LATEST is pushed as link address
+    comma()
+    dictionary[3] = len(dictionary) - 1 # LATEST is updated
 
-def print_dictionary():
-    for i, cell in enumerate(dictionary):
-        print(i, cell)
+def store():
+    address = pop()
+    contents = pop()
+    dictionary[address] = contents
 
+def fetch():
+    address = pop()
+    contents = dictionary[address]
+    push(contents)
 
-# NATIVE DICTIONARY FUNCTIONS
-def variable():
-    print('IN VARIABLE index, index2: ', index, index2, '   dictionary[2]: ', dictionary[2])
-    push(index2-1)
+################### NATIVE STACK MANIPULATION FUNCTIONS ###################
 
 def dup():
-    top = stack[SP]
+    top = stack[-1]
     push(top)
     next_word()
 
 def rot():
-    """
-    1 2 3
-    2 3 1
-    """
+    # 1 2 3 -> 2 3 1
     three = pop()
     two = pop()
     one = pop()
@@ -116,10 +123,7 @@ def rot():
     push(one)
 
 def swap():
-    """
-    2 1
-    1 2
-    """
+    # 2 1 -> 1 2
     one = pop()
     two = pop()
     push(one)
@@ -150,60 +154,60 @@ def div():
     next_word()
 
 def equals():
-    print('\n in equals ')
     a = pop()
     b = pop()
-    print('a: ', a, type(a), ' b: ', b, type(b))
-    print(' a == b ? : ', a==b)
     if a == b:
         push(1)
     else:
         push(0)
-    print_stack()
     next_word()
 
-def store():
-    address = pop()
-    contents = pop()
-    print('IN STORE addr,contents : ', address, contents, ' index, index2: ', index, index2, '   dictionary[2]: ', dictionary[2])
-    dictionary[address] = contents
+def lessthan():
+    b = pop()
+    a = pop()
+    if a < b:
+        push(1)
+    else:
+        push(0)
+    next_word()
 
-def fetch():
-    address = pop()
-    contents = dictionary[address]
-    print('IN FETCH addr,contents : ', address, contents, ' index, index2: ', index, index2, '   dictionary[2]: ', dictionary[2])
-    push(contents)
+def greaterthan():
+    b = pop()
+    a = pop()
+    if a > b:
+        push(1)
+    else:
+        push(0)
+    next_word()
+
+def ifdup():
+    top = stack[SP]
+    if top != 0:
+        push(top)
+    next_word()
+
+################### COMPILING AND INTERPRETING ##############################
 
 def exit():
-    print('IN EXIT top of RS : ', return_stack[-1], ' index, index2: ', index, index2, '   dictionary[2]: ', dictionary[2])
-    #global dictionary[2]
     tors = pop_RS()
-    #push(tors)
-    #set_dictionary[2]()
-    dictionary[2] = tors
-    #if dictionary[2] != None:
-    #    next_word()
+    dictionary[5] = tors
+    if dictionary[5] != -1:
+        next_word()
 
 def enter():
-    print('IN ENTER, dictionary[2]  and index: ', ' index, index2: ', index, index2, '   dictionary[2]: ', dictionary[2])
-    #global dictionary[2]
-    push_RS(dictionary[2])
-    dictionary[2] = index
-    #print('#####in enter, dictionary[2]  and index: ', dictionary[2], index, 'type dictionary[2]', type(dictionary[2]))
+    push_RS(dictionary[5])
+    dictionary[5] = index
     next_word()
 
 def next_word():
-    print('IN NEXT WORD, ', ' index, index2: ', index, index2, '   dictionary[2]: ', dictionary[2])
-    #global dictionary[2]
-    if dictionary[2] != -1: # so that it only does anything if we r in a thread
+    if dictionary[5] != -1: # so that it only does anything if we r in a thread
         # there must be a more elegant solution?!?!
-        dictionary[2] = dictionary[2] + 1
-        push(dictionary[2])
+        dictionary[5] = dictionary[5] + 1
+        push(dictionary[5])
         execute()
 
 def execute():
     tos = pop()
-    print('IN EXECUTE tos: ', tos, ' index, index2: ', index, index2, '   dictionary[2]: ', dictionary[2])
     # index and index2 used because no way to reliably get address r index from value.
     # So we need another way to keep track of where in the dictionary we are and how we got there.
     global index
@@ -219,31 +223,34 @@ def execute():
         # The cell contains the index of another cell, which contains the function pointer we want.
         index = dictionary[tos]
 
-    dictionary[index]()
+    word_trace.append([[index2, index, dictionary[index]], [dictionary[5]], stack[:], return_stack[:]]) # for debgging
+
+    dictionary[index]() # execute the function we want
 
 def get_word():
     global input_stream
     new_word = ""
-    while len(input_stream):
-        new_word = new_word + input_stream[0]
+    c = ""
+    while c != " " and len(input_stream) > 0:
+        c = input_stream[0]
+        new_word = new_word + c
         input_stream = input_stream[1:]
-        if input_stream[0] == " ":
-            input_stream = input_stream[1:]
-            push(new_word)
-            return
     push(new_word)
 
 def find():
-    print('in FIND')
+    #print('(((((((((((((((( in FIND ', dictionary[3])
     new_word = pop()
-    current_link_index = latest
-    while current_link_index != None:
+    #print('new word is ', new_word)
+    current_link_index = dictionary[3] #LATEST
+    while current_link_index != -1:
+        #print('current_link_index : ', current_link_index)
         current_name = dictionary[current_link_index-2]
-        #print('\n\n in find, looping through linked list')
-        #print('word to find, current name, current_link_index : ', new_word, current_name, current_link_index)
-        if current_name== new_word:
+        #print('current name : ', current_name, ' new_word: ', new_word)
+        ##print('\n\n in find, looping through linked list')
+        ##print('word to find, current name, current_link_index : ', new_word, current_name, current_link_index)
+        if current_name.strip() == new_word.strip():
             push(current_link_index + 1)
-            #print('FOUND: word and link to be sent for it: ', new_word, current_link_index+1)
+            ##print('FOUND: word and link to be sent for it: ', new_word, current_link_index+1)
             #print_dictionary()
             if dictionary[current_link_index-1] == 1:
                 push(-1)
@@ -255,206 +262,67 @@ def find():
     push(0)
 
 def number():
-    print('in NUMBER')
     string = pop()
     number = int(string)
     push(number)
 
 def interpret_word():
     get_word()
-    print('/n in interpret word ', stack[-1])
+    word = stack[-1]
     find()
-    print_stack()
     tos = pop()
     if tos == 1 or tos is -1:
         execute()
     else:
         number()
 
-def ifdup():
-    top = stack[SP]
-    if top != 0:
-        push(top)
-    next_word()
-
-def comma():
-    print('IN COMMA')
-    tos = pop()
-    dictionary.append(tos)
-
 def set_interpret():
-    print('set state -> 0')
     global STATE
     STATE = 0
 
 def set_compile():
-    print('set state -> 1')
     global STATE
     STATE = 1
 
 def colon():
-    print('IN COMPILE')
     set_compile()
-    global latest
-    push(None)
-    comma()
-    push(0) # how can immediate flag be set to 0 for a dynamically added word?
-    comma()
-    push(latest)
-    comma()
+    create('/', 0)
 
-    placer_for_latest = len(dictionary) - 1 #  index of last cell added (holds link address to previous entry)
-    push(enter)
-    comma()
-
-    latest = placer_for_latest
-    here = len(dictionary) - 1
-
-"""
-: COLON
-    SET-COMPILE
-    // get rid of all the placer for latest stuff by using , to add everything to dict?
-    // create will use , and , will update here
-    CREATE
-    @ ENTER ,
-
-
-: SET-COMPILE 1 STATE ! ;
-
-: SET-INTERPRET 0 STATE ! ;
-"""
 def semicolon():
-    print('IN SEMI COLOn!')
     set_interpret()
-    #dictionary.append(exit)
     push(exit)
     comma()
 
 def compile_word():
-    print('IN COMPILE WORD')
     get_word()
     word = pop()
-
-    push(word)
-    find()
-
-    found = pop()
-
-    print('\n\n\n in compile word, word and found : ', word, found)
-
-    if found < 0:
-        print('immediate flag, will execute')
-        execute()
-
-    elif dictionary[-4] == None:
-        print('creat header')
-        dictionary[-4] = word
-        pop()
-
-    elif found == 1:
+    if dictionary[-3] == '/':
+        dictionary[-3] = word
+        push(enter)
         comma()
-
     else:
-        literal()
+        push(word)
+        find()
+        found = pop()
+        if found < 0: # word has an immediate flag
+            execute()
+        elif found == 1: # no immediate flag, word should be compiled
+            comma()
+        else:
+            literal() # word isn't found, it is a number
 
-
-"""def dictionary[2]func(): #this is what dictionary[2] would do in forth?!
-    print('IN dictionary[2] FUNC')
-    push('dictionary[2]')
-    find()
-
-def get_dictionary[2]():
-    push('dictionary[2]')
-    find()
-
-def set_dictionary[2]():
-    push('dictionary[2]')
-    find()
-    pop()
-    push(1)
-    add()
-    swap()
-    store()
-"""
 def doliteral():
-    print('\n\nin doliteral, index and index2 and dictionary[2] : ', index, index2, dictionary[2])
-    print('will push: ', (dictionary[index2+1]))
     push(int(dictionary[index2+1]))
-    #global dictionary[2]
-    dictionary[2] = dictionary[2] + 1
-    print_stack()
+    dictionary[5] = dictionary[5] + 1
     next_word()
 
 def literal():
-    print('IN LITERAL')
     x = pop()
     dictionary.append(doliteral)
     dictionary.append(x)
 
-
-def if_():
-    print('IN IF')
-    dictionary.append(Qbranch)
-    dictionary.append(None)
-    push(len(dictionary)-1) ##### CHECK THIS
-
-def else_():
-    print('IN ELSE')
-    dictionary.append(branch)
-    #print('in else')
-    #print_dictionary()
-    dictionary.append(None)
-    push(len(dictionary)-1)
-    #print(';len dict : ', len(dictionary))
-    #push(len(dictionary)-2) ##### CHECK THIS
-
-def Qbranch():
-    #global dictionary[2]
-    dictionary[2] = dictionary[2] + 1
-    x = pop()
-    push_RS(x)
-    print('in qbranch') #, PC +1, x :', PC, x)
-    if x != 0: #true - carry out first block
-        print('TRUE')
-        dictionary[2] = dictionary[2] + 1
-        push(dictionary[2])
-        execute()
-    """else:
-        print('FALSE')
-        PC = dictionary[PC]
-        push(PC)
-        execute()"""
-
-def branch():
-    #global PC
-    print('in branch')#, PC is', PC)
-    x = pop_RS()
-    if x == 0:
-        print('FALSE')
-        dictionary[2] = dictionary[2] + 2
-        push(dictionary[2])
-        execute()
-    else:
-        print('TRUE')
-        dictionary[2] = dictionary[dictionary[2]+1]
-        push(dictionary[2])
-        execute()
-
-def then():
-    print('\n\n\n IN THEN, THE STACK: ')
-    print_stack()
-    dup()
-    push(1)
-    sub()
-    rot()
-    rot()
-    print_stack()
-    print('len(dictionary) is ', len(dictionary) )
-    push(len(dictionary))
-    swap()
-    print_stack()
-    store()
-    store()
+def variable():
+    push(index2-1)
 
 def quit():
     global return_stack
@@ -465,7 +333,87 @@ def quit():
         else:
             interpret_word()
 
-# Add some words to dictionary
+
+################### BRANCHING AND LOOPING ##############################
+
+def if_():
+    push(Qbranch)
+    comma()
+    push(None)
+    comma()
+    push(len(dictionary)-1) ##### CHECK THIS
+
+def else_():
+    push(branch)
+    comma()
+    push(None)
+    comma()
+    push(len(dictionary)-1)
+
+def Qbranch():
+    dictionary[5] = dictionary[5] + 1 # increment the PC
+    x = pop()
+    push_RS(x) # store in return stack so branch can check it later
+    if x != 0: # true - carry out first block
+        dictionary[5] = dictionary[5] + 1 # PC increments by one to enter first block
+        push(dictionary[5])
+        execute()
+    else:
+        dictionary[5] = dictionary[dictionary[5]] # PC becomes index saved in cell after qbranch
+        push(dictionary[5])
+        execute()
+
+
+def branch():
+    x = pop_RS()
+    if x == 0: # will carry out second block
+        dictionary[5] = dictionary[5] + 2
+        push(dictionary[5])
+        execute()
+    else:
+        dictionary[5] = dictionary[dictionary[5]+1]
+        push(dictionary[5])
+        execute()
+
+def then():
+    dup()
+    push(1)
+    sub()
+    rot()
+    rot()
+    push(len(dictionary))
+    swap()
+    store()
+    store()
+
+def begin_():
+    push_RS(index2) # store BEGIN's addr in the RS, so we can return to it if we repeat the loop
+    next_word()
+
+def until():
+    addr = pop_RS()
+    flag = pop()
+    compare = pop()
+    if flag != compare:
+        #print('continue')
+        dictionary[5] = addr - 1 # set PC to one less than BEGIN's addr
+    next_word()
+
+def while_():
+    addr = pop_RS()
+    flag = pop()
+    compare = pop()
+    if flag == compare:
+        #print('continue')
+        dictionary[5] = addr - 1 # set PC to one less than BEGIN's addr
+    next_word()
+
+def bye():
+    global running
+    running = False
+
+
+################ ADD NATIVE WORDS TO DICTIONARY ####################
 add_word('.S', 0, print_stack, [])
 add_word('DUP', 0, dup, [])
 add_word('*', 0, mul, [])
@@ -473,47 +421,49 @@ add_word('+', 0, add, [])
 add_word('-', 0, sub, [])
 add_word('/', 0, div, [])
 add_word('=', 0, equals, [])
+add_word('<', 0, lessthan, [])
+add_word('>', 0, greaterthan, [])
 add_word(':', 1, colon, [])
 add_word(';', 1, semicolon, [])
 add_word('!', 0, store, [])
 add_word('@', 0, fetch, [])
 add_word('FIND', 0, find, [])
-add_word('NUMBER', 1, number, [])
-add_word('>R', 1, push_RS, [])
-add_word('R>', 1, pop_RS, [])
-add_word(',', 1, comma, [])
-#add_word('PC', 0, variable, None)
+add_word('NUMBER', 1, number, None)
+add_word('>R', 1, push_RS, None)
+add_word('R>', 1, pop_RS, None)
+add_word(',', 1, comma, None)
 add_word('IF', 1, if_, None)
 add_word('ELSE', 1, else_, None)
 add_word('THEN', 1, then, None)
 add_word('BRANCH', 0, branch, None)
 add_word('QBRANCH', 0, Qbranch, None)
+add_word('BEGIN', 0, begin_, None)
+add_word('WHILE', 0, while_, None)
+add_word('UNTIL', 0, until, None)
+add_word('[', 0, set_interpret, None)
+add_word(']', 0, set_compile, None)
+add_word('BYE', 0, bye, None)
 
-### TESTS
-def test_add_words():
-    push(1)
-    push(2)
-    push(3)
-    print('sp: ', stack[SP])
-    pop()
-    print_stack()
-    print('sp: ', stack[SP])
-    dup()
-    mul()
-    print_stack()
+def print_cw(): # FOR DEBUGGING
+    print(' [index2, index, dictionary[index]]            [PC] [stack]   [return_stack] ')
+    for line in word_trace:
+        print(line)
 
 
-def test_linked_list():
-    print('Test the linked list:')
-    current = latest
-    print(current)
-    while current != None:
-        current = dictionary[current]
-        print(current)
+if __name__ == "__main__":
+    input_stream = ""
 
-input_stream = ": TEST 8 = IF 7 DUP ELSE 100 * THEN ; 5 8 TEST "
-#input_stream = ": TEST IF 7 DUP ELSE 100 * THEN ; 5 1 TEST "
-#input_stream = ": TEST IF 7 ELSE 100 THEN ; 1 TEST "
-#input_stream = ": TWICE DUP + DUP ; : SQUARED DUP * ; : CUBED DUP SQUARED * ; 2 CUBED 3 SQUARED 5 TWICE .S "
-quit()
-print_dictionary
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        filetext = open(filename, 'r')
+        for line in filetext:
+            input_stream = input_stream + line.strip() + " "
+        #input_stream = filetext
+        print('INPUT STREAM: ', input_stream)
+
+    quit()
+    running = True
+    while running:
+        input_stream = input()
+        if input_stream:
+            quit()
---
+++ test_forth.py
@@ -0,0 +1,116 @@
+import pytest
+import forth
+
+class TestStacks:
+    def test_param_stack_push_and_pop(self):
+        forth.push(1)
+        forth.push(2)
+        forth.push(3)
+        assert forth.pop() == 3
+        assert forth.pop() == 2
+        assert forth.pop() == 1
+
+    def test_return_stack_push_and_pop(self):
+        forth.push_RS(1)
+        forth.push_RS(2)
+        forth.push_RS(3)
+        assert forth.pop_RS() == 3
+        assert forth.pop_RS() == 2
+        assert forth.pop_RS() == 1
+
+class TestDictionary:
+    def test_add_tracking_variables_start_of_dictionary(self):
+        assert forth.dictionary[1] == len(forth.dictionary) - 1 #HERE
+        assert forth.dictionary[5] == -1
+        assert forth.dictionary[7] == -1
+        assert forth.dictionary[9] == -1
+
+    def test_linked_list(self):
+        current = forth.dictionary[3]
+        addresses = []
+        while current != -1:
+            current = forth.dictionary[current]
+            addresses.append(current)
+        assert min(addresses) == -1
+        assert max(addresses) < len(forth.dictionary)
+        print(addresses, '\n', sorted(addresses, reverse=True))
+        assert addresses == sorted(addresses, reverse=True)
+
+
+class TestThreading:
+    def test_define_word(self):
+        forth.input_stream = ": TWICE DUP + DUP ; 5 TWICE "
+        forth.quit()
+        assert forth.pop() == 10
+        assert forth.pop() == 10
+
+    def test_define_multiple_words(self):
+        forth.input_stream = ": DOUBLE DUP + ; : TWICE DOUBLE DUP ; : SQUARED DUP * ; : CUBED DUP SQUARED * ; 2 CUBED 3 SQUARED 5 TWICE .S "
+        forth.quit() #8 9 10 10
+        assert forth.pop() == 10
+        assert forth.pop() == 10
+        assert forth.pop() == 9
+        assert forth.pop() == 8
+
+    def test_if_else(self):
+        forth.input_stream = ": TEST IF 7 ELSE 100 THEN ; 1 0 TEST "
+        forth.quit()
+        assert forth.pop() == 100
+        assert forth.dictionary[-10].__name__ == "enter"
+        assert forth.dictionary[-9].__name__ == "Qbranch"
+        assert forth.dictionary[-7].__name__ == "doliteral"
+        assert int(forth.dictionary[-6]) == 7
+        assert forth.dictionary[-5].__name__ == "branch"
+        assert forth.dictionary[-3].__name__ == "doliteral"
+        assert int(forth.dictionary[-2]) == 100
+        assert forth.dictionary[-1].__name__ == "exit"
+        forth.input_stream = ": TEST DUP 7 < IF DUP 6 DUP ELSE 100 * THEN ; 5 TEST"
+        forth.quit()
+        assert forth.pop() == 6
+        assert forth.pop() == 6
+        assert forth.pop() == 5
+        assert forth.pop() == 5
+
+
+
+    def test_while(self):
+        forth.input_stream = ": TEST 5 BEGIN DUP 1 + DUP 20 < 1 WHILE ; TEST "
+        forth.stack = []
+        forth.quit()
+        assert forth.stack == [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
+"""def test_until(self):
+        forth.input_stream = ": TEST 5 BEGIN DUP 1 + DUP 20 UNTIL ; TEST"
+        forth.stack = []
+        forth.quit()
+        assert forth.stack == [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+"""
+
+
+
+
+
+
+"""### TESTS
+def test_add_words():
+    push(1)
+    push(2)
+    push(3)
+    #print('sp: ', stack[SP])
+    pop()
+    print_stack()
+    #print('sp: ', stack[SP])
+    dup()
+    mul()
+    print_stack()
+
+
+def test_linked_list():
+    #print('Test the linked list:')
+    current = dictionary[3]
+    #print(current)
+    while current != None:
+        current = dictionary[current]
+        #print(current)
+"""
