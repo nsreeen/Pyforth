a219c2f2927d234834dc85c89b0fd46b5a2ef9a7
Nasreen123 <Nasreen123@users.noreply.github.com>
2017-06-30 11:04:37 +0100

    Use checkbox in enter to submit form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import Form
-from wtforms import RadioField, TextAreaField
+from wtforms import BooleanField, TextAreaField
 from wtforms.validators import DataRequired
 
 class ModeForm(Form):
-        mode = RadioField('mode', choices=[('compile', 'compile'),('interpret', 'interpret')], default='compile')
+    mode = BooleanField()
 
 class CompileForm(Form):
     input_stream = TextAreaField('input_string', validators=[DataRequired()])
